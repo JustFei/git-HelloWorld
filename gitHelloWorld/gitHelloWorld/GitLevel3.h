b23c53ab7361ddd5980a428685044d9cae7d8669
xxf <kaixiaofei@gmail.com>
2016-06-04 19:07:32 +0800

    gitLevel3 -- 7:07

---
+++ gitHelloWorld/gitHelloWorld/GitLevel3.h
@@ -0,0 +1,17 @@
+//
+//  GitLevel3.h
+//  gitHelloWorld
+//
+//  Created by mac on 16/6/4.
+//  Copyright © 2016年 com.qianfeng.xxf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GitLevel3 : NSObject
+
+@property (nonatomic ,copy) NSString *name3;
+
+@property (nonatomic ,copy) NSString *age3;
+
+@end
---
+++ gitHelloWorld/gitHelloWorld/GitLevel3.m
@@ -0,0 +1,18 @@
+//
+//  GitLevel3.m
+//  gitHelloWorld
+//
+//  Created by mac on 16/6/4.
+//  Copyright © 2016年 com.qianfeng.xxf. All rights reserved.
+//
+
+#import "GitLevel3.h"
+
+@implementation GitLevel3
+
+- (void)GitLevel3
+{
+    NSLog(@"这里是分时gitLevel3的内容! -- 7:06");
+}
+
+@end
