356c6d26f52508714369f51e511289bde872b85d
xxf <kaixiaofei@gmail.com>
2016-06-04 18:52:11 +0800

    GitLevel2 -- 6:52

---
+++ gitHelloWorld/gitHelloWorld/GitLevel2.h
@@ -0,0 +1,17 @@
+//
+//  GitLevel2.h
+//  gitHelloWorld
+//
+//  Created by mac on 16/6/4.
+//  Copyright © 2016年 com.qianfeng.xxf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GitLevel2 : NSObject
+
+@property (nonatomic ,copy)NSString *name;
+
+@property (nonatomic ,copy)NSString *age;
+
+@end
---
+++ gitHelloWorld/gitHelloWorld/GitLevel2.m
@@ -0,0 +1,18 @@
+//
+//  GitLevel2.m
+//  gitHelloWorld
+//
+//  Created by mac on 16/6/4.
+//  Copyright © 2016年 com.qianfeng.xxf. All rights reserved.
+//
+
+#import "GitLevel2.h"
+
+@implementation GitLevel2
+
+- (void)GitLevel2
+{
+    NSLog(@"gitlevel2 -- 6:50");
+}
+
+@end
